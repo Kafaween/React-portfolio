167882835ae42a5cf04d8afaf16e5ec4c9d08f8c
Kafaween <abod.kafaween@gmail.com>
2021-07-31 08:04:53 +0300

    done React portofolio

--- src/components/Footers/SimpleFooter.js
+++ src/components/Footers/SimpleFooter.js
@@ -5,10 +5,10 @@
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Copyright 2020Abed Kafaween (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/main/LICENSE.md)
 
-* Coded by Creative Tim
+* Coded byAbed Kafaween
 
 =========================================================
 
@@ -16,7 +16,7 @@
 
 */
 /*eslint-disable*/
-import React from "react";
+import React from 'react';
 // reactstrap components
 import {
   Button,
@@ -26,127 +26,127 @@ import {
   Container,
   Row,
   Col,
-  UncontrolledTooltip
-} from "reactstrap";
+  UncontrolledTooltip,
+} from 'reactstrap';
 
 class SimpleFooter extends React.Component {
   render() {
     return (
       <>
-        <footer className=" footer">
+        <footer className=' footer'>
           <Container>
-            <Row className=" row-grid align-items-center mb-5">
-              <Col lg="6">
-                <h3 className=" text-primary font-weight-light mb-2">
+            <Row className=' row-grid align-items-center mb-5'>
+              <Col lg='6'>
+                <h3 className=' text-primary font-weight-light mb-2'>
                   Thank you for supporting us!
                 </h3>
-                <h4 className=" mb-0 font-weight-light">
+                <h4 className=' mb-0 font-weight-light'>
                   Let's get in touch on any of these platforms.
                 </h4>
               </Col>
-              <Col className="text-lg-center btn-wrapper" lg="6">
+              <Col className='text-lg-center btn-wrapper' lg='6'>
                 <Button
-                  className="btn-icon-only rounded-circle"
-                  color="twitter"
-                  href="https://twitter.com/creativetim"
-                  id="tooltip475038074"
-                  target="_blank"
+                  className='btn-icon-only rounded-circle'
+                  color='twitter'
+                  // href="https://twitter.com/creativetim"
+                  id='tooltip475038074'
+                  target='_blank'
                 >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-twitter" />
+                  <span className='btn-inner--icon'>
+                    <i className='fa fa-twitter' />
                   </span>
                 </Button>
-                <UncontrolledTooltip delay={0} target="tooltip475038074">
+                <UncontrolledTooltip delay={0} target='tooltip475038074'>
                   Follow us
                 </UncontrolledTooltip>
                 <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="facebook"
-                  href="https://www.facebook.com/creativetim"
-                  id="tooltip837440414"
-                  target="_blank"
+                  className='btn-icon-only rounded-circle ml-1'
+                  color='facebook'
+                  href='https://www.linkedin.com/in/abdel-rahman-kafawen-10364320a/'
+                  id='tooltip837440414'
+                  target='_blank'
                 >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-facebook-square" />
+                  <span className='btn-inner--icon'>
+                    <i className='fa fa-linkedin-square' />
                   </span>
                 </Button>
-                <UncontrolledTooltip delay={0} target="tooltip837440414">
+                <UncontrolledTooltip delay={0} target='tooltip837440414'>
                   Like us
                 </UncontrolledTooltip>
                 <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="dribbble"
-                  href="https://dribbble.com/creativetim"
-                  id="tooltip829810202"
-                  target="_blank"
+                  className='btn-icon-only rounded-circle ml-1'
+                  color='dribbble'
+                  // href='https://dribbble.com/creativetim'
+                  id='tooltip829810202'
+                  target='_blank'
                 >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-dribbble" />
+                  <span className='btn-inner--icon'>
+                    <i className='fa fa-dribbble' />
                   </span>
                 </Button>
-                <UncontrolledTooltip delay={0} target="tooltip829810202">
+                <UncontrolledTooltip delay={0} target='tooltip829810202'>
                   Follow us
                 </UncontrolledTooltip>
                 <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="github"
-                  href="https://github.com/creativetimofficial"
-                  id="tooltip495507257"
-                  target="_blank"
+                  className='btn-icon-only rounded-circle ml-1'
+                  color='github'
+                  href='https://github.com/Kafaween'
+                  id='tooltip495507257'
+                  target='_blank'
                 >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-github" />
+                  <span className='btn-inner--icon'>
+                    <i className='fa fa-github' />
                   </span>
                 </Button>
-                <UncontrolledTooltip delay={0} target="tooltip495507257">
+                <UncontrolledTooltip delay={0} target='tooltip495507257'>
                   Star on Github
                 </UncontrolledTooltip>
               </Col>
             </Row>
             <hr />
-            <Row className=" align-items-center justify-content-md-between">
-              <Col md="6">
-                <div className=" copyright">
-                  © {new Date().getFullYear()}{" "}
+            <Row className=' align-items-center justify-content-md-between'>
+              <Col md='6'>
+                <div className=' copyright'>
+                  © {new Date().getFullYear()}{' '}
                   <a
-                    href="https://www.creative-tim.com?ref=adsr-footer"
-                    target="_blank"
+                    href='https://www.creative-tim.com?ref=adsr-footer'
+                    target='_blank'
                   >
-                    Creative Tim
+                    Abed Kafaween
                   </a>
                   .
                 </div>
               </Col>
-              <Col md="6">
-                <Nav className=" nav-footer justify-content-end">
+              <Col md='6'>
+                <Nav className=' nav-footer justify-content-end'>
                   <NavItem>
                     <NavLink
-                      href="https://www.creative-tim.com?ref=adsr-footer"
-                      target="_blank"
+                      href='https://www.creative-tim.com?ref=adsr-footer'
+                      target='_blank'
                     >
-                      Creative Tim
+                      Abed Kafaween
                     </NavLink>
                   </NavItem>
                   <NavItem>
                     <NavLink
-                      href="https://www.creative-tim.com/presentation?ref=adsr-footer"
-                      target="_blank"
+                      href='https://www.creative-tim.com/presentation?ref=adsr-footer'
+                      target='_blank'
                     >
                       About Us
                     </NavLink>
                   </NavItem>
                   <NavItem>
                     <NavLink
-                      href="http://blog.creative-tim.com?ref=adsr-footer"
-                      target="_blank"
+                      href='http://blog.creative-tim.com?ref=adsr-footer'
+                      target='_blank'
                     >
                       Blog
                     </NavLink>
                   </NavItem>
                   <NavItem>
                     <NavLink
-                      href="https://github.com/creativetimofficial/argon-design-system-react/blob/main/LICENSE.md"
-                      target="_blank"
+                      href='https://github.com/creativetimofficial/argon-design-system-react/blob/main/LICENSE.md'
+                      target='_blank'
                     >
                       MIT License
                     </NavLink>
